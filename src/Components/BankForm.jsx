99b3286ce4266533796add88df5f17ed440432b7
Soumojit <tikudatta2050@gmail.com>
2023-08-09 15:01:45 +0530

    mobile padding fix

--- src/Components/BankForm.jsx
+++ src/Components/BankForm.jsx
@@ -3,25 +3,22 @@ import TextInput from "./TextInput";
 import { useState } from "react";
 
 export default function BankForm() {
- 
-    const [currentDate, setCurrentDate] = useState('');
-    const [inputValue, setInputValue] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
 
-    const [save, setSave] = useState(false);
+  const [save, setSave] = useState(false);
 
-  
-    const handleButtonClick = (e) => {
-      e.preventDefault();
-      const now = new Date();
-      const formattedDate = now.toDateString().toUpperCase();
-     
-      setCurrentDate(formattedDate);
-      setSave(!save);
-    };
-  
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    const now = new Date();
+    const formattedDate = now.toDateString().toUpperCase();
+
+    setCurrentDate(formattedDate);
+    setSave(!save);
+  };
+
   return (
     <>
-      <div className="flex flex-col m-10 ">
+      <div className="flex flex-col m-5 sm:m-10 ">
         <div className="container w-[100%]">
           <div className="title font-sans-serif font-medium text-4xl leading-normal">
             Update Bank Details
@@ -35,55 +32,53 @@ export default function BankForm() {
         </div>
         <form className="container mt-6 border p-5 rounded-lg w-[100%]">
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4">
-            
             <label className="font-bold ml-4 " htmlFor="acname">
               ACCOUNT HOLDER NAME
             </label>
-            <TextInput placeholder="Account Holder Name" save={save} inputValue={inputValue => setInputValue(inputValue)}/>
+            <TextInput placeholder="Account Holder Name" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4 ">
             <label className="font-bold ml-4" htmlFor="acNo">
               ACCOUNT NUMBER
             </label>
-            <TextInput placeholder="Account Number" save={save}/>
+            <TextInput placeholder="Account Number" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4">
             <label className="font-bold ml-4" htmlFor="accode">
               IFSC CODE
             </label>
-            <TextInput placeholder="IFSC Code" save={save}/>
+            <TextInput placeholder="IFSC Code" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4">
             <label className="font-bold ml-4" htmlFor="bankname">
               BANK NAME
             </label>
-            <TextInput placeholder="Bank Name" save={save}/>
+            <TextInput placeholder="Bank Name" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4">
             <label className="font-bold ml-4" htmlFor="bankcity">
               BANK CITY
             </label>
-            <TextInput placeholder="Bank City" save={save}/>
+            <TextInput placeholder="Bank City" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4">
             <label className="font-bold ml-4" htmlFor="branchname">
               BRANCH NAME
             </label>
-            <TextInput placeholder="Branch Name" save={save}/>
+            <TextInput placeholder="Branch Name" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mr-4 mb-4 ">
             <label className="font-bold ml-4" htmlFor="acrelation">
               RELATION WITH ACCOUNT HOLDER
             </label>
-            <TextInput placeholder="Relation with Account Holder" save={save}/>
+            <TextInput placeholder="Relation with Account Holder" save={save} />
           </div>
           <div className="flex flex-col sm:flex-row sm:justify-between">
             <label className="font-bold ml-4 mb-4" htmlFor="acname">
               CONSENT
             </label>
             <div className="mb-4 sm:mr-4">
-              <Checkbox currentDate = {currentDate} save={save}/>
-            
+              <Checkbox currentDate={currentDate} save={save} />
             </div>
           </div>
           <div className="flex justify-end mb-4">
@@ -91,7 +86,9 @@ export default function BankForm() {
               onClick={handleButtonClick}
               type="submit"
               value="Save"
-              className={`border w-20 p-2 text-center sm:mr-4 rounded-md bg-shade-one cursor-pointer text-white ${save&&inputValue?"hidden":""}`}
+              className={`border w-20 p-2 text-center sm:mr-4 rounded-md bg-shade-one cursor-pointer text-white ${
+                save && inputValue ? "hidden" : ""
+              }`}
             />
           </div>
         </form>
@@ -99,7 +96,7 @@ export default function BankForm() {
           THE ABOVE DETAILS ARE FINAL AND WILL BE USED FOR PAYMENT. IF ANY OF
           THESE DETAILS ARE WRONG, PLEASE CONTACT YOUR MANAGER IMMEDIATELY! ALSO
           EMAIL THE SAME TO ACCOUNTS@EXAMBAZAAR.COM!
-        <div className="border m-4 bg-primary-text"></div>
+          <div className="border m-4 bg-primary-text"></div>
         </span>
       </div>
     </>
